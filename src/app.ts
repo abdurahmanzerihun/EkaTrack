653111e385b2ffeb65d45ea4f9c886cb601267cb
abdurahman <abdurrahmanzerihun@gmail.com>
2026-06-19 16:03:15 +0300

    Seeding setups

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,14 @@
-import { prisma } from "./lib/prisma";
+import express,{Request,Response} from 'express';
+const app =express();
+const PORT=process.env.PORT||4000;
 
+app.use(express.json());
+
+app.get('/',(req:Request,res:Response)=>{
+        res.send("The shop server is working ");
+        
+});
+
+app.listen(PORT,()=>{
+        console.log(`The app is working on port ${PORT}` );
+});
