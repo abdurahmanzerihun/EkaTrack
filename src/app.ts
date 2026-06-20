eb598279bdb6ebb8b80fe98b6d838fa2432389ff
abdurahman <abdurrahmanzerihun@gmail.com>
2026-06-20 19:18:32 +0300

    Add packages and define the login api in the app.ts file

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,27 @@
-import express,{Request,Response} from 'express';
+import express,{Request,Response,NextFunction} from 'express';
 const app =express();
 const PORT=process.env.PORT||4000;
 
+//import routes
+import authRoutes from './routes/authRoutes';
+
+//middlewares
 app.use(express.json());
 
+
+app.use('/api/auth/',authRoutes);
 app.get('/',(req:Request,res:Response)=>{
         res.send("The shop server is working ");
         
+});
+
+//async catch
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  console.error("System Error:", err.message);
+  
+  res.status(500).json({ 
+    message: "An unexpected database or server error occurred." 
+  });
 });
 
 app.listen(PORT,()=>{
