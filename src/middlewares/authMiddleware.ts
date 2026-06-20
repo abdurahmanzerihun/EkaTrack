6276d708d8eee53c1af46a347b214a0d75713249
abdurahman <abdurrahmanzerihun@gmail.com>
2026-06-20 23:03:32 +0300

    Add route protecting middleware

---
+++ src/middlewares/authMiddleware.ts
@@ -0,0 +1,39 @@
+import { prisma } from "../lib/prisma";
+import { Request,Response,NextFunction } from "express";
+import  jwt  from "jsonwebtoken";
+import { Role } from "../generated/prisma/enums";
+
+interface CustomJwtPayload{
+        userId:string,
+        role:Role,
+        username:string
+}
+
+export const authenticateUser=(req:Request,res:Response,next:NextFunction) =>{
+try{
+        const header=req.headers.authorization;
+        if(!header || !header.startsWith("Bearer ")){
+        res.status(401).json({message:"No token provided or the token does not include the Bearer"});
+        return;
+
+}
+
+const token=header.split(" ")[1];
+const JWT_SECRET=process.env.JWT_SECRET;
+if(!JWT_SECRET){
+        throw new Error("FATAL ERROR: JWT_SECRET is not defined in the environment variables.");
+}
+
+const decoded=jwt.verify(token,JWT_SECRET);
+req.user=decoded as CustomJwtPayload;
+next()
+
+}
+catch(error){
+        res.status(401).json({message:"The token is invalid or expired"});
+}
+
+}
+export const authorizeUser=(req:Request,res:Response)=>{
+
+}
