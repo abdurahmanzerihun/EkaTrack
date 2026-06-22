23bb8ec034fa0eb9901976888d876f9346832078
abdurahman <abdurrahmanzerihun@gmail.com>
2026-06-22 15:36:19 +0300

    Admin creates staff

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,7 +1,11 @@
 import { Request,Response,NextFunction } from "express";
 import { prisma } from "../lib/prisma";
 import { compare } from "bcrypt";
+import crypto from "crypto";
 import jwt  from "jsonwebtoken";
+import { Role } from "../generated/prisma/enums";
+import bcrypt from 'bcrypt';
+
 
 export const login=async(req:Request,res:Response,next:NextFunction): Promise<void> =>{
         try{
@@ -18,14 +22,25 @@ const user=await prisma.user.findUnique({
         }
 }
 );
-if(!user || ! (await compare(password,user.password))){
-        res.status(400).json({message:"Invalid User or Password"});
-        return;
+// Checking if the user exists first
+if (!user) {
+    res.status(400).json({ message: "Invalid User or Password" });
+    return ;
+}
+
+//Compare the password 
+
+const isMatch = await bcrypt.compare(password, user.password);
+
+if (!isMatch) {
+    res.status(400).json({ message: "Invalid Password" }); 
+    return ;
 }
 
 //Is the user active 
 if(!user.isActive){
-        res.status(403).json({message:"You have been deactivated"})
+        res.status(403).json({message:"You have been deactivated"});
+        return;
 }
 
 //jwt token generation
@@ -49,3 +64,85 @@ res.status(200).json({
 
 
 };
+
+export const registerStaff=async (req:Request,res:Response,next:NextFunction):Promise<void> =>{
+
+try{
+        const {firstName ,lastName,phoneNumber,gender,email,role}=req.body;
+//validating the req.body
+
+ if(!firstName || !lastName){
+        res.status(400).json({message:"First Name or LastName field missing"});
+        return;
+}
+        if(!email || !role){
+                res.status(400).json({message:"Email or Role field missing"})
+                return ;
+        }
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+        if(!emailRegex.test(email)){
+                res.status(400).json({message:"The email is invalid"});
+                return ;
+        }
+
+        //checking if the email already exists 
+
+        const normalizedEmail=email.toLowerCase().trim();
+
+        const existingUser=await prisma.user.findUnique({
+                where:{
+                        email:normalizedEmail
+                }
+        });
+        if(existingUser){
+                res.status(409).json({message:"The email already exists "});
+                return ;
+        }
+
+//temporary password and username generations
+        const OTP=crypto.randomBytes(4).toString("hex");
+        console.log(`OTP:${OTP}`);
+        const hashedPassword=await bcrypt.hash(OTP,10);
+        const prefix=email.split('@')[0];
+        const generatedUsername=`${prefix}_${crypto.randomBytes(2).toString("hex")}`;
+
+//user account and profile creation with transaction
+const result=await prisma.$transaction(async(tx)=>{
+    const staffAccount=await tx.user.create({
+                data:{
+                   email:normalizedEmail,
+                   username:generatedUsername,
+                   role,
+                   firstLogin:true,
+                   password:hashedPassword
+                },
+        });
+        
+        const staffProfile=await tx.profile.create({
+                data:{
+                firstName,
+                lastName,
+                phoneNumber,
+                gender,
+                userId:staffAccount.id
+                }
+
+        }) ;
+
+        return {staffAccount,staffProfile}
+})
+       
+     res.status(200).json({
+                message:"Staff Onboarded Succesfully",
+                firstName:result.staffProfile.firstName,
+                lastName:result.staffProfile.lastName,
+                username:result.staffAccount.username,
+                email:result.staffAccount.email,
+                OTP
+ })
+
+}catch(error){
+        next(error);
+}
+}
--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,7 +1,10 @@
 import { Router } from "express";
 const router=Router();
-import { login } from "../controllers/authController";
+import { login,registerStaff } from "../controllers/authController";
+import { authenticateUser, authorizeUser } from "../middlewares/authMiddleware";
+import { Role } from "../generated/prisma/enums";
 
 router.post('/login',login);
+router.post('/register-staff',authenticateUser,authorizeUser(Role.ADMIN),registerStaff);
 
 export default router;
