2fc153c213eef21cf9e007b353d044c9049bf65e
abdurahman <abdurrahmanzerihun@gmail.com>
2026-06-27 21:08:52 +0300

    Define the product creation route in the category route file

--- src/routes/category-routes.ts
+++ src/routes/category-routes.ts
@@ -3,9 +3,16 @@ const router=Router();
 import { createCategory, deleteCatagory, getCategory, updateCategory } from "../controllers/category-controller";
 import { authenticateUser, authorizeUser } from "../middlewares/auth-middleware";
 import { Role } from "../generated/prisma/enums";
+import { validate } from "../middlewares/validation-middleware";
+import { categoryParamsSchema,updateCategorySchema, createCategorySchema } from "../validations/category-validations";
+import { createProductSchema } from "../validations/product-validation";
+import { createProduct } from "../controllers/product-controller";
 
-router.post('/',authenticateUser,authorizeUser(Role.ADMIN),createCategory);
+router.post('/',authenticateUser,authorizeUser(Role.ADMIN),validate(createCategorySchema),createCategory);
 router.get('/',authenticateUser,authorizeUser(Role.ADMIN,Role.MANAGER,Role.STAFF),getCategory);
-router.patch('/:id',authenticateUser,authorizeUser(Role.ADMIN),updateCategory);
-router.delete('/:id',authenticateUser,authorizeUser(Role.ADMIN),deleteCatagory);
+router.patch('/:id',authenticateUser,authorizeUser(Role.ADMIN),validate(updateCategorySchema),updateCategory);
+router.delete('/:id',authenticateUser,authorizeUser(Role.ADMIN),validate(categoryParamsSchema),deleteCatagory);
+//A route for creating products for specific category
+router.post('/:categoryId/products',authenticateUser,authorizeUser(Role.ADMIN),validate(createProductSchema),createProduct);
+
 export default router;
