de27828ef9fdc0438f0520b4df27b9ad65ae72e7
abdurahman <abdurrahmanzerihun@gmail.com>
2026-06-27 22:25:15 +0300

    Request body validation for staff registration and  login

--- src/routes/auth-routes.ts
+++ src/routes/auth-routes.ts
@@ -3,8 +3,10 @@ const router=Router();
 import { login,registerStaff } from "../controllers/auth-controller";
 import { authenticateUser, authorizeUser } from "../middlewares/auth-middleware";
 import { Role } from "../generated/prisma/enums";
+import { validate } from "../middlewares/validation-middleware";
+import { loginSchema, registerStaffSchema } from "../validations/auth-validation";
 
-router.post('/login',login);
-router.post('/register-staff',authenticateUser,authorizeUser(Role.ADMIN),registerStaff);
+router.post('/login',validate(loginSchema),login);
+router.post('/register-staff',authenticateUser,authorizeUser(Role.ADMIN),validate(registerStaffSchema),registerStaff);
 
 export default router;
